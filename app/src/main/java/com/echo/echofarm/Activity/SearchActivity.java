a8f6f49a716dac5fcc0a3547729af2021b86e443
Chanho lee <devchlee12@gmail.com>
2022-11-22 16:55:17 +0900

    찬호-게시글 확인 액티비티 추가

---
+++ app/src/main/java/com/echo/echofarm/Activity/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.echo.echofarm.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.echo.echofarm.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
---
+++ app/src/main/java/com/echo/echofarm/Activity/TradeListAdapter.java
@@ -0,0 +1,5 @@
+package com.echo.echofarm.Activity;
+
+public class TradeListAdapter {
+
+}
